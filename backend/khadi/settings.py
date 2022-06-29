9387596fecdad58e34b59f5b2647cb2223b18f8d
Aswanth C P <aswanthcp1996@gmail.com>
2022-06-29 22:27:07 +0530

    change in backend

--- backend/khadi/settings.py
+++ backend/khadi/settings.py
@@ -74,18 +74,12 @@ WSGI_APPLICATION = 'khadi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'khadi_products',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
